883bb2bd5a89c35e49c18737c0a104a28859dd34
Divesh Podar <poddardivesh@gmail.com>
2025-06-22 15:25:48 +0530

    #6 Dependency Injection using Spring Boot
    Annotations :
        @SpringBootApplication
        @Component

---
+++ src/main/java/com/nevermind/SpringCourse/SpringCourseApplication.java
@@ -0,0 +1,18 @@
+package com.nevermind.SpringCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringCourseApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(SpringCourseApplication.class, args);
+
+		Dev dev = context.getBean(Dev.class);
+		dev.build();
+	}
+
+}
